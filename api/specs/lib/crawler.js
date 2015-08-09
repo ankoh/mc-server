79c6fd7340f0edc03d73e7efde546cf9528237a1
Andre Kohn <andre@kohn.io>
2015-08-09 17:02:17 +0200

    Removing the javascript stuff

--- api/specs/lib/crawler.js
+++
@@ -1,7 +0,0 @@
-var MendeleyCrawler = require('../../lib/crawler');
-
-describe('new MendeleyCrawler(validAppId, validClientSecret)', function () {
-    it(' shall ', function () {
-        
-    });
-});
