92b0c5dcfb29940a025a289bebafb7c478f83820
Andre Kohn <andre@kohn.io>
2015-08-09 11:54:58 +0200

    Initial project structure

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/*jslint node: true, indent: 2 */
+'use strict';
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg     : grunt.file.readJSON('package.json'),
+    jshint  : {
+      all     : ['package.json', 'Gruntfile.js', 'index.js', 'routes/**/*.js', 'common/**/*.js', 'tests/**/*.js']
+    },
+    jasmine_nodejs : {
+      options : {
+        specNameSuffix: 'spec.js'
+      },
+      all     : {
+        specs : [
+          "test/*.spec.js"
+        ]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jasmine-nodejs');
+  grunt.loadNpmTasks('grunt-serve');
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'jshint',
+    'jasmine_nodejs'
+  ]);
+
+};
+
--- README.md
+++
@@ -1,2 +0,0 @@
-
-[![build status](https://gitlab-ci.kohn.io/projects/15/status.png?ref=master)](https://gitlab-ci.kohn.io/projects/15?ref=master)
