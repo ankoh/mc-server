f4bd025d59fbd0582b9430eff86305ad65c7d9c3
Andre Kohn <andre@kohn.io>
2015-08-23 12:04:18 +0200

    Testing different sql query options

--- mendeleycache/test/data/test_config.py
+++ mendeleycache/test/data/test_config.py
@@ -13,11 +13,29 @@ class TestConfig(unittest.TestCase):
         engine = create_engine(sqlite_in_memory)
         self.assertIsNotNone(engine)
 
+        # Option 1: Explicit connection
         conn = engine.connect()
         conn.execute("CREATE TABLE x (a INTEGER, b INTEGER)")
         conn.execute("INSERT INTO x (a, b) VALUES (1, 1)")
         conn.execute("INSERT INTO x (a, b) VALUES (2, 2)")
         result = conn.execute("SELECT x.a, x.b FROM x")
         conn.close()
+        self.assertEqual(result.keys(), ["a", "b"])
 
-        assert result.keys() == ["a", "b"]
+        # Option 2: connect with statement
+        with engine.connect() as conn:
+            conn.execute("DELETE FROM x")
+            conn.execute("INSERT INTO x (a, b) VALUES (42, 43)")
+            results = conn.execute("SELECT x.a, x.b FROM x")
+            for row in results:
+                self.assertEqual(row['a'], 42)
+                self.assertEqual(row['b'], 43)
+
+        # Option 3: transaction with begin
+        with engine.begin() as conn:
+            conn.execute("DELETE FROM x")
+            conn.execute("INSERT INTO x (a, b) VALUES (42, 43)")
+            results = conn.execute("SELECT x.a, x.b FROM x")
+            for row in results:
+                self.assertEqual(row['a'], 42)
+                self.assertEqual(row['b'], 43)
