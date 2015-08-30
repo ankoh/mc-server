af6cefa882ebbeaa24d3046a8cd7f5d4d495afd8
Andre Kohn <andre@kohn.io>
2015-08-30 18:14:05 +0200

    Added CORS to app.py

--- mendeleycache/app.py
+++ mendeleycache/app.py
@@ -1,5 +1,9 @@
 __author__ = 'kohn'
 
 from mendeleycache.mendeleycache import MendeleyCache
+from flask.ext.cors import CORS
 
 app = MendeleyCache(__name__)
+
+# Allow cross origin site requests on all routes
+CORS(app)
