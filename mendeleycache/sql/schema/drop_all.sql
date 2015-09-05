fff9cafc3c62f13b08b23ad0ab8a2e5dd2028cf8
Andre Kohn <andre@kohn.io>
2015-09-05 10:31:03 +0200

    Changing table drop order on schema change

--- mendeleycache/sql/schema/drop_all.sql
+++ mendeleycache/sql/schema/drop_all.sql
@@ -1,8 +1,8 @@
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS document;
 DROP TABLE IF EXISTS cache_profile;
 DROP TABLE IF EXISTS cache_document;
 DROP TABLE IF EXISTS cache_field;
 DROP TABLE IF EXISTS cache_profile_has_cache_document;
 DROP TABLE IF EXISTS cache_document_has_cache_field;
-DROP TABLE IF EXISTS profile;
-DROP TABLE IF EXISTS document;
 DROP TABLE IF EXISTS update_log;
